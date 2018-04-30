3a8b72ba2bec18222ceaf4ded461a9672c9fb10b
Sup Tans <s.tansutirapong@gmail.com>
2018-05-01 00:12:02 +0700

    add 3 endpoints

--- ideaboard/src/components/IdeasContainer.js
+++ ideaboard/src/components/IdeasContainer.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import update from 'immutability-helper'
 import axios from 'axios'
+import Idea from './Idea'
+import IdeaForm from './IdeaForm'
 
 class IdeasContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-        ideas: []
+        ideas: [],
+        editingIdeaId: null,
+        notification: ''
     }
   }
 
@@ -13,7 +18,64 @@ class IdeasContainer extends Component {
     axios.get('http://localhost:3001/api/v1/ideas.json')
     .then(response => {
       console.log(response)
-      this.setState({ideas: response.data})
+      this.setState({
+        ideas: response.data,
+        editingIdeaId: response.data.id
+      })
+      
+    })
+    .catch(error => console.log(error))
+  }
+
+  addNewIdea = () => {
+    axios.post(
+      'http://localhost:3001/api/v1/ideas',
+      { idea:
+        {
+          title: '',
+          body: ''
+        }
+      }
+    )
+    .then(response => {
+      console.log(response)
+      const ideas = update(this.state.ideas, {
+        $splice: [[0, 0, response.data]]
+      })
+      this.setState({
+        ideas: ideas,
+        editingIdeaId: response.data.id
+      })
+    })
+    .catch(error => console.log(error))
+  }
+
+  updateIdea = (idea) => {
+    const ideaIndex = this.state.ideas.findIndex(x => x.id === idea.id)
+    const ideas = update(this.state.ideas, {
+      [ideaIndex]: { $set: idea }
+    })
+    this.setState({
+      ideas: ideas,
+      notification: 'All changes saved'
+    })
+  }
+
+  resetNotification = () => {
+    this.setState({notification: ''})
+  }
+
+  enableEditing = (id) => {
+    this.setState({editingIdeaId: id},
+      () => { this.title.focus() })
+  }
+
+  deleteIdea = (id) => {
+    axios.delete(`http://localhost:3001/api/v1/ideas/${id}`)
+    .then(response => {
+      const ideaIndex = this.state.ideas.findIndex(x => x.id === id)
+      const ideas = update(this.state.ideas, { $splice: [[ideaIndex, 1]]})
+      this.setState({ideas: ideas})
     })
     .catch(error => console.log(error))
   }
@@ -21,7 +83,25 @@ class IdeasContainer extends Component {
   render() {
     return (
       <div>
-        Ideas
+        <button className="newIdeaButton"
+          onClick={this.addNewIdea} >
+          New Idea
+        </button>
+        <span className="notification">
+          {this.state.notification}
+        </span>
+        {this.state.ideas.map((idea) => {
+          if(this.state.editingIdeaId === idea.id) {
+            return(<IdeaForm idea={idea} key={idea.id}
+              titleRef= {input => this.title = input}
+              updateIdea={this.updateIdea}
+              resetNotification={this.resetNotification} />)
+          } else {
+            return (<Idea idea={idea} key={idea.id}
+              onClick={this.enableEditing}
+              onDelete={this.deleteIdea} />)
+          }      
+        })}
       </div>
     )
   }
